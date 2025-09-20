346920d6d802c53a7c1e69fdfae2346691a93860
aidanjamai <aidansjamai99@gmail.com>
2025-09-20 12:58:09 -0400

    PR fix

--- backend/internal/api/market/dto.go
+++ backend/internal/api/market/dto.go
@@ -1,17 +1,14 @@
 package market
 
-import "time"
-
 type PostStockRequest struct {
 	Symbol string  `json:"symbol"`
 	Price  float64 `json:"price"`
 }
 
 type StockResponse struct {
-	Symbol    string    `json:"symbol"`
-	Date      string    `json:"date"`
-	Price     float64   `json:"price"`
-	Timestamp time.Time `json:"timestamp"`
+	Symbol string  `json:"symbol"`
+	Date   string  `json:"date"`
+	Price  float64 `json:"price"`
 }
 
 type StockIdRequest struct {
